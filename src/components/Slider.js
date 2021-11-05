bbaf1bbe3537e6a6c895acfaed0b53c9b28f2f35
AzharAkhter123 <87418312+AzharAkhter123@users.noreply.github.com>
2021-11-05 19:23:11 +0500

    asd
    asd

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,6 +4,20 @@ export const Slider=()=>{
     return(
         <section className="slider-section">
             <div className="overlay"></div>
+           <div className="slider-content">
+               <div className="container">
+                   <div className="row">
+                       <div className="col-md-12">
+                           <div className="card-show">
+                               <p>price</p>
+                               <h3>ALATO CABINET</h3>
+                               <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+                               <button>Purchase Now</button>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
         </section>
     )
 }
