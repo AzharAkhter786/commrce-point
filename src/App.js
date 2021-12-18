a856c0d0710c8e5948708556ebb233a95572d591
AzharAkhter786 <azharakhteroffical@gmail.com>
2021-12-18 18:56:51 +0500

    asdas
    xzxzx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import './styles/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Header} from './components/Header';
-import {Slider} from './components/Slider';
-import {Gallery} from './components/Gallery';
-import {Heading} from './components/Heading';
-import {Banner} from './components/Banner';
+import {HomePage} from './pages/Home/index'
+import {ShopPage} from './pages/shop/index'
+import {AboutPage} from './pages/about/index'
+import {CartPage} from './pages/cart/index'
+import {CheckoutPage} from './pages/checkout/index'
+import {LoginPage} from './pages/login/index'
+import {ProductDetailPage} from './pages/product.detail/index'
 import {Footer} from './components/Footer';
-import {NewsLetter} from './components/NewsLetter';
-import {Counter} from './components/Counter';
-import {HappyClient} from './components/HappyClient';
+import {Header} from './components/Header';
+
+// import {Slider} from './components/Slider';
+// import {Services} from './components/Services';
+// import {Products} from './components/Products';
+
+// import {NewsLetter} from './components/NewsLetter';
+// import {Counter} from './components/Counter';
+// import {HappyClient} from './components/HappyClient';
+
 
 function App() {
 
   return (
     <>
     <Header/>
-    <Slider/>
-    <Gallery/>
-    
-    <HappyClient/>
-    <Counter/>
-    <NewsLetter/>
+    <HomePage/>
+    <ShopPage/>
+    <AboutPage/>
+    <CartPage/>
+    <CheckoutPage/>
+    <ProductDetailPage/>
+    <LoginPage/>
+    <Footer/>
+  
     <Footer/>
    </>
   );
